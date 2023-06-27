0fb1a6358169cfde0078ccd408898cb27331ce4d
Jamalay <dzamaevmansur12@gmail.com>
2023-06-27 21:29:48 +0300

    First Game on js

--- tamogotchi.js
+++ tamogotchi.js
@@ -3,13 +3,19 @@ const tamogotchi = {
     meal: 1,
     energy: 2,
     mood: 4,
+    isDead: false,
+    //inteligence: 1,
     getStatus: function(){
         let hungry = '';
         let tired = '';
         let inTheMood = '';
         if (this.meal <= 0 || this.energy <= 0 || this.mood <= 0){
+            this.isDead = true;
             console.log(`[${this.name}]: умер :(`);
         }
+        /*else if(this.inteligence <= 0){
+            console.log('ваш персонаж слишком тупой');
+        }*/
         else{
             if (this.meal < 3){ hungry = 'Я голоден'; }
             else{  hungry = 'Я не голоден'; }
@@ -24,28 +30,46 @@ const tamogotchi = {
         this.name = name;
     },
     Eat: function(){
-        let k = 0;
-        if (k >= 5){
-            this.meal++;
-            k = 0;
+        if (this.isDead){
+            console.log('вы мертвы, вызовите метод "reset" стобы начать занаво')
         }
-        if (this.meal < 5){
+        else if (this.meal < 5){
             this.meal++;
             this.mood--;
-            k++;
+        }
+        else{
+            console.log('Вы Сыты');
         }
     },
     Sleep: function(){
-        if (this.energy < 5){
+        if (this.isDead){
+            console.log('вы мертвы, вызовите метод "reset" стобы начать занаво')
+        }
+        else if (this.energy < 5){
             this.energy++;
             this.meal--;
         }
+        else{
+            console.log('Вы поспали');
+        }
     },
     Play: function(){
-        if (this.mood < 5){
+        if (this.isDead){
+            console.log('вы мертвы, вызовите метод "reset" стобы начать занаво')
+        }
+        else if (this.mood < 5){
             this.mood++;
             this.energy--;
         }
+        else{
+            console.log('Вы уже поиграли');
+        }
+    },
+    reset: function(){
+        this.meal = 1;
+        this.energy = 2;
+        this.mood = 4;
+        console.log('Игра перезапущена')
     }
   }
 
@@ -92,6 +116,20 @@ tamogotchi.getStatus();
 tamogotchi.Eat();
 
 tamogotchi.getStatus();
+
+tamogotchi.Eat();
+
+tamogotchi.getStatus();
+
+tamogotchi.Eat();
+
+tamogotchi.getStatus();
+
+tamogotchi.Eat();
+
+tamogotchi.getStatus();
+
+tamogotchi.reset();
 
 tamogotchi.Eat();
 
